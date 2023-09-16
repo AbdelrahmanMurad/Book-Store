205dbbf3525ff6d9d1df82939e9f230f4883949a
AbdelrahmanMurad <awmurad2001@gmail.com>
2023-09-17 01:30:16 +0300

    join/aggregate in login() between users & reviewers collections

--- Models/User.js
+++ Models/User.js
@@ -93,8 +93,9 @@ class User {
 
     /**login() method
      * 1- make a validation operation.
-     * 2- dbConnection to fetch username & password by findOne
-     * 3- compare between passwords
+     * 2- dbConnection
+     * 3- Join - aggregate: merging the data we need between two collections (users & reviewers)
+     * 4- compare between passwords
      */
     static login(loginData) {
         return new Promise((resolve, reject) => {
@@ -109,27 +110,66 @@ class User {
                 })
             }
 
-            //dbConnection to fetch username & password by findOne
             dbConnection('users', async (collection) => {
                 try {
-                    const user = await collection.findOne(
-                        { username: loginData.username },
-                        { projection: { username: 1, password: 1 } }
-                    )
+                    /** aggregate([]) => output: array of objects => merging the data we need between two collections (users & reviewers)
+                     *   - it will take 3 objects:
+                     *       - lookup object: takes multiple fields.
+                     *               - form field: Name of related collection: we need to merge the Related Collection (second collection) with the Main Collection (first collection).
+                     *               - localField: name of key in the Main Collection. => in SQL: Primary Key
+                     *               - foreignField: name of key in the Related Collection. => in SQL: Foreign Key
+                     *               - as field: name of related document => انت حر في اختيار الاسم
+                     *       - match object: condition
+                     *       - limit object: to limit the number of users in result.
+                     * =======================================================================
+                     *  The problems in aggregate method: 
+                     *      1- findOne()وليس ال find()انها تتعامل مع الداتا مثل ال 
+                     *          - المطلوب فقط userمش ال usersهيك هي بتجيب كل ال 
+                     *          - Solution: (match & limit objects) => match obj for make a condition  + limit obj for specifying number of users.
+                     *      2- arrayواحنا بدنا نشيل ال arrayانها بتجيب الداتا في ال (data in list)
+                     *          - Solution: select the first object in the array to each collection, so the output will be (object) not (array of objects).
+                     *                      - first user: const user = dbResult[0];
+                     *                      - first reviewer: user.reviewer = (user.reviewer) ? user.reviewer[0] : null
+                     */
+                    const dbResutl = await collection.aggregate([
+                        {
+                            $lookup: {
+                                from: 'reviewers',
+                                localField: '_id',
+                                foreignField: '_user_id',
+                                as: 'reviewer'
+                            }
+                        },
+                        { $match: { username: loginData.username } },
+                        { $limit: 1 }
+                    ]).toArray()
 
-                    //compare between passwords
-                    if (user) {
-                        if (compareSync(loginData.password, user.password) == true) {
+                    //Start of parent if
+                    if (dbResutl) {
+                        //first user (first object)
+                        const user = dbResutl[0];
+
+                        //loginData.password => decrypted
+                        //user.password => encrypted
+                        if (user || compareSync(loginData.password, user.password)) {
+                            //first reviewer (first object)
+                            // if user.reviewer exist
+                            user.reviewer = (user.reviewer) ? user.reviewer[0] : null
+                            // or => if (user.reviewer) { user.reviewer = user.reviewer[0] } else { user.reviewer = null }
+                        } else {
                             resolve({
-                                status: true,
-                                data: user
+                                status: false
                             })
                         }
+
+                        //End of parent if
+                        resolve({
+                            status: true,
+                            data: user
+                        })
                     } else {
                         resolve({
-                            status: false,
-                            message: 'login failed',
-                            code: 401
+                            status: false
                         })
                     }
                 } catch (error) {
@@ -138,6 +178,7 @@ class User {
                         message: error.message
                     })
                 }
+
             })
 
         })
